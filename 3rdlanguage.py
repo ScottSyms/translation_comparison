4018d3eabfe490c748c367469af25c8a7ef6e01f
Scott Syms <scottsyms@gmail.com>
2022-03-07 22:00:54 -0400

    refactored script

--- 3rdlanguage.py
+++ 3rdlanguage.py
@@ -1,15 +1,68 @@
+from gettext import translation
+from webbrowser import get
 import requests
+import re
 import uuid
 import json
 import spacy
 from string import punctuation
-import urllib.request
+# import urllib.request
 from bs4 import BeautifulSoup
 import fr_core_news_lg
-import de_core_news_lg
+import es_core_news_lg
+
+
+# Retrieve a web page
+def get_webpage_text(url):
+    # Fetch two sample pages
+    page=requests.get(url)
+
+    # extract the text from the pages
+    page=BeautifulSoup(page.text, features="lxml").get_text()
+
+    # Strip off excess line endings 
+    return re.sub(r'\n+', '\n', page)
+
+# Translate text from source language to Spanish
+def get_translation(text, source_language):
+    # Translate the French page into Spanish
+    params = {
+        'api-version': '3.0',
+        'from': source_language,
+        'to': ['es']
+    }
+
+    # Pass the text to the translation service and return it
+    body = [{
+        'text': text
+    }]
+    request = requests.post(constructed_url, params=params, headers=headers, json=body)
+    return request.json()[0]["translations"][0]["text"]
+
+# Extract the significant words from the text
+def isolate_words(text):
+    # Extract all the significant words from the source English text
+    keywords={}
+    if text.lang_=="en":
+        stopwords=list(spacy.lang.en.stop_words.STOP_WORDS)
+    elif text.lang_=="fr":
+        stopwords=list(spacy.lang.fr.stop_words.STOP_WORDS)
+    else:
+        stopwords=list(spacy.lang.es.stop_words.STOP_WORDS)
+    pos_tag = ['VERB', 'NOUN', 'ADV', 'ADJ']
+    for token in text:
+            if(token.text in stopwords or token.text in punctuation):
+                continue
+            if(token.pos_ in pos_tag):
+                    if (token.lemma_ in keywords.keys()):
+                        keywords[token.lemma_]=keywords[token.lemma_]+1
+                    else:
+                        keywords[token.lemma_]=1
+    return keywords
 
 
 # Set up the Microsoft Translation boiler plate
+# Taken from Translation services example
 # **************************************************
 # Add your subscription key and endpoint
 subscription_key = "c3e034153d3f4842bd4db3886b75e5b2"
@@ -30,126 +83,40 @@ headers = {
     'X-ClientTraceId': str(uuid.uuid4())
 }
 
-
-# Load the French and English tokenization lists
+# Load the language token lists
 nlpen = spacy.load("en_core_web_lg")
 nlpfr= fr_core_news_lg.load()
-nlpde=de_core_news_lg.load()
+nlpes=es_core_news_lg.load()
 
-# Fetch two sample pages
-# fren  ch=urllib.request.urlopen("https://www.canada.ca/fr/ministere-defense-nationale/organisation/politiques-normes/directives-ordonnances-administratives-defense/serie-5000/5031/5031-50-apprentissage-et-perfectionnement-professionnel-du-personnel-civil.html")
-# english=urllib.request.urlopen("https://www.canada.ca/en/department-national-defence/corporate/policies-standards/defence-administrative-orders-directives/5000-series/5031/5031-50-civilian-learning-and-professional-development.html")
-
-english=urllib.request.urlopen("https://www.canada.ca/en/department-national-defence/corporate/policies-standards/defence-administrative-orders-directives/1000-series/1002/1002-1-requests-under-privacy-act-personal-information.html")
-# french=urllib.request.urlopen("https://www.canada.ca/fr/ministere-defense-nationale/organisation/politiques-normes/directives-ordonnances-administratives-defense/serie-1000/1002/1002-1-demandes-de-renseignements-personnels-vertu-loi-protection-des-renseignements-personnels.html")
-french=urllib.request.urlopen("https://chipwired.com/programming-arduino-with-rust/")
-
-# extract the data from the pages and remove line breaks
-english=BeautifulSoup(english, features="lxml").get_text().replace("\n", " ")
-french=BeautifulSoup(french, features="lxml").get_text().replace("\n", " ")
+# Get the pages
+english=get_webpage_text("https://www.canada.ca/en/department-national-defence/corporate/policies-standards/defence-administrative-orders-directives/1000-series/1002/1002-1-requests-under-privacy-act-personal-information.html")
+french=get_webpage_text("https://www.canada.ca/fr/ministere-defense-nationale/organisation/politiques-normes/directives-ordonnances-administratives-defense/serie-1000/1002/1002-1-demandes-de-renseignements-personnels-vertu-loi-protection-des-renseignements-personnels.html")
 
 
-# Translate the French page into German
-params = {
-    'api-version': '3.0',
-    'from': 'fr',
-    'to': ['de']
-}
-# Pass the French text to the translation service and isolate the text
-body = [{
-    'text': french
-}]
-request = requests.post(constructed_url, params=params, headers=headers, json=body)
-translated_from_french = request.json()[0]["translations"][0]["text"]
-
-
-# Translate the English text to French
-params = {
-    'api-version': '3.0',
-    'from': 'en',
-    'to': ['de']
-}
-# Pass the English text to the translation service and isolate the text
-body = [{
-    'text': english
-}]
-request = requests.post(constructed_url, params=params, headers=headers, json=body)
-translated_from_english = request.json()[0]["translations"][0]["text"]
-
+# Translate them
+translated_from_english=get_translation(english, "en")
+translated_from_french=get_translation(french, "fr")
 
 # Parse all texts in the language parser
-original_french=nlpfr(french)
-original_english=nlpen(english)
-french_from_english=nlpde(translated_from_french)
-english_from_french=nlpde(translated_from_english)
-
-# Extract all the significant words from the source English text
-keywords_original_english={}
-stopwords=list(spacy.lang.en.stop_words.STOP_WORDS)
-pos_tag = ['VERB', 'NOUN', 'ADV']
-for token in original_english:
-           if(token.text in stopwords or token.text in punctuation):
-             continue
-           if(token.pos_ in pos_tag):
-                if (token.lemma_ in keywords_original_english.keys()):
-                    keywords_original_english[token.lemma_]=keywords_original_english[token.lemma_]+1
-                else:
-                    keywords_original_english[token.lemma_]=1
-
-# Extract all the significant words from the source French text
-keywords_original_french={}
-stopwords=list(spacy.lang.fr.stop_words.STOP_WORDS)
-pos_tag = ['VERB', 'NOUN', 'ADV']
-for token in original_french:
-           if(token.text in stopwords or token.text in punctuation):
-             continue
-           if(token.pos_ in pos_tag):
-                if (token.lemma_ in keywords_original_french.keys()):
-                    keywords_original_french[token.lemma_]=keywords_original_french[token.lemma_]+1
-                else:
-                    keywords_original_french[token.lemma_]=1
-
-# Extract all the significant words fromt the German translated from English
-keywords_translated_from_french={}
-stopwords=list(spacy.lang.de.stop_words.STOP_WORDS)
-pos_tag = ['VERB', 'NOUN', 'ADV']
-for token in french_from_english:
-           if(token.text in stopwords or token.text in punctuation):
-             continue
-           if(token.pos_ in pos_tag):
-                if (token.lemma_ in keywords_translated_from_french.keys()):
-                    keywords_original_french[token.lemma_]=keywords_translated_from_french[token.lemma_]+1
-                else:
-                    keywords_translated_from_french[token.lemma_]=1
-
-# Extract all the significant words from the German translated from the French
-keywords_translated_from_english={}
-stopwords=list(spacy.lang.de.stop_words.STOP_WORDS)
-pos_tag = ['VERB', 'NOUN', 'ADV']
-for token in english_from_french:
-           if(token.text in stopwords or token.text in punctuation):
-             continue
-           if(token.pos_ in pos_tag):
-                if (token.lemma_ in keywords_translated_from_english.keys()):
-                    keywords_translated_from_english[token.lemma_]=keywords_translated_from_english[token.lemma_]+1
-                else:
-                    keywords_translated_from_english[token.lemma_]=1
+original_french_keywords=isolate_words(nlpfr(french))
+original_english_keywords=isolate_words(nlpen(english))
+translation_from_english_keywords=isolate_words(nlpes(translated_from_french))
+translation_from_french_keywords=isolate_words(nlpes(translated_from_english))
 
 
-comparison=set(keywords_translated_from_french.keys()).intersection(set(keywords_original_french.keys()))
-# english_comparison=set(keywords_translated_from_english.keys()).intersection(set(keywords_original_english.keys()))
+comparison=set(translation_from_french_keywords.keys()).intersection(set(translation_from_english_keywords.keys()))
 
-print("How many words in the original English", len(keywords_original_english))
-print("How many words in the original French", len(keywords_original_french))
-print("How many words in the English translation", len(keywords_translated_from_english))
-print("How many words in the French Translation", len(keywords_translated_from_french))
+print("How many words in the original English", len(original_english_keywords))
+print("How many words in the original French", len(original_french_keywords))
+print("How many words in the English translation", len(translation_from_english_keywords))
+print("How many words in the French Translation", len(translation_from_french_keywords))
 print("How many words shared between  translations.", len(comparison))
-# print("How many words shared between the French translations", len(french_comparison))
-print("Simularity ", (len(comparison)*2/(len(keywords_original_english)+len(keywords_translated_from_french)))*100)
-# print("Simularity French", len(french_comparison)*2/(len(keywords_original_french)+len(keywords_translated_from_english)))
+
+# similarity score is the comparison value multiplied by 2 and divided by the total number of keywords for French and English source.
+print("Simularity: %5.2f" % ((len(comparison)*2/(len(translation_from_english_keywords)+len(translation_from_french_keywords)))*100))
 
 # Open a file and save some diagnostic data
-f=open("summary_trans.txt", "w")
+f=open("translation_summary.txt", "w")
 f.write(english + "\n\n\n")
 f.write(translated_from_french + "\n\n\n")
 f.write(french + "\n\n\n")
@@ -158,19 +125,20 @@ f.write(translated_from_english + "\n\n\n")
 
 
 # Write the sorted word lists
-f.write("Original English")
-for i,j in sorted(keywords_original_english.items()) :
-    f.write(i+"\n")
+f.write("Original English\n")
+for i,j in sorted(original_english_keywords.items(), key=lambda item: item[1], reverse=True):
+    f.write(str(j)+" " +i+"\n")
 
-f.write("\n\n\n\nTranslated English")
-for i,j in sorted(keywords_translated_from_french.items()):
-    f.write(i+"\n")
+f.write("\n\n\n\nTranslated English\n")
+for i,j in sorted(translation_from_english_keywords.items(), key=lambda item: item[1], reverse=True): 
+    f.write(str(j)+" " +i+"\n")
 
-f.write("\n\n\n\Original French")
-for i,j in sorted(keywords_original_french.items()):
-    f.write(i+"\n")
+f.write("\n\n\n\Original French\n")
+for i,j in sorted(original_french_keywords.items(), key=lambda item: item[1], reverse=True): 
+    f.write(str(j)+" " +i+"\n")
 
-f.write("\n\n\n\Translated French")
-for i,j in sorted(keywords_translated_from_english.items()):
-    f.write(i+"\n")
+f.write("\n\n\n\Translated French\n")
+for i,j in sorted(translation_from_french_keywords.items(), key=lambda item: item[1], reverse=True): 
+    f.write(str(j)+" " +i+"\n")
+
 f.close()   
